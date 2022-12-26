465c8e8efa4b8a55ca84aa004b98582423c4939c
befilz <117735865+befilz@users.noreply.github.com>
2022-12-26 15:15:01 +0700

    Stack
    send a homework

---
+++ stackApp/stackApp/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace stackApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Stack stack;
+        public MainWindow()
+        {
+            InitializeComponent();
+            stack = new Stack();
+        }
+
+        private void bttnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            stack.Push(txtValue.Text);
+            txtValue.Clear();
+
+            foreach (string data in stack)
+            {
+                MessageBox.Show(data);
+                
+            }
+
+        }
+    }
+}
